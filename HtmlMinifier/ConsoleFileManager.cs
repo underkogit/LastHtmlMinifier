06129a0699f0345a239e215d7b2f70fb3f0c8882
UnderKo <UnderKo@DESKTOP-P1BPFCJ>
2025-11-06 03:02:18 +0300

    Test

---
+++ HtmlMinifier/ConsoleFileManager.cs
@@ -0,0 +1,34 @@
+﻿namespace HtmlMinifier;
+
+public static class ConsoleFileManager
+{
+    public static string GetFilePath(string storageFile)
+    {
+        var filePath = string.Empty;
+
+        if (File.Exists(storageFile))
+        {
+            filePath = File.ReadAllText(storageFile);
+            Console.WriteLine($"Найден сохранённый путь к файлу: {filePath}");
+        }
+
+        while (true)
+        {
+            if (string.IsNullOrEmpty(filePath))
+            {
+                Console.WriteLine("Введите путь к файлу: ");
+                filePath = Console.ReadLine().Replace("\"", string.Empty).Trim();
+            }
+
+            if (File.Exists(filePath))
+            {
+                Console.WriteLine("Файл существует. Сохраняем путь.");
+                File.WriteAllText(storageFile, filePath);
+                return filePath;
+            }
+
+            Console.WriteLine("Файл не найден. Пожалуйста, попробуйте снова.");
+            filePath = string.Empty;
+        }
+    }
+}
---
+++ HtmlMinifier/Program.cs
@@ -0,0 +1,66 @@
+﻿using HtmlMinifier;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var nodeRunner = new NodeRunner();
+        var sotorageDirectoryScript = Path.GetFullPath("Scripts");
+        var directoryCache = Path.GetFullPath("cache");
+
+
+        string outputCppHFile = "C:\\Users\\UnderKo\\Documents\\PlatformIO\\Projects\\ESP32WebPanel\\include\\webui.h";
+        string outputHtmlViewTest = Path.GetFullPath("test.html");
+        if (!Directory.Exists(sotorageDirectoryScript))
+        {
+            Directory.CreateDirectory(sotorageDirectoryScript);
+        }
+        if (!Directory.Exists(directoryCache))
+        {
+            Directory.CreateDirectory(directoryCache);
+        }
+
+        var sotorageNodeScriptHtml = Path.Combine(sotorageDirectoryScript, @"MinifierHTML.js");
+        var sotorageNodeScriptJs = Path.Combine(sotorageDirectoryScript, @"MinifierJS.js");
+        var storageFile = "filePath.txt";
+        var inputPath = ConsoleFileManager.GetFilePath(storageFile);
+
+        void Build()
+        {
+            var pathMiniJs = nodeRunner.CreateScriptsJs(inputPath, directoryCache, sotorageNodeScriptJs);
+            nodeRunner.RunNodeScript(sotorageNodeScriptJs);
+            var pathMiniHtml = nodeRunner.CreateScriptsHtml(inputPath, directoryCache, sotorageNodeScriptHtml);
+            nodeRunner.RunNodeScript(sotorageNodeScriptHtml);
+            nodeRunner.CopressCodeToFile(pathMiniJs, pathMiniHtml, outputHtmlViewTest);
+            
+            string code = nodeRunner.CopressCode(pathMiniJs, pathMiniHtml);
+            File.WriteAllText(outputCppHFile, nodeRunner.GetCppH(code));
+
+        }
+
+        if (!string.IsNullOrEmpty(inputPath))
+        {
+            var directoryPath = Path.GetDirectoryName(inputPath);
+            Console.WriteLine($"Отслеживание изменений в директории: {directoryPath}");
+            Build();
+
+            using (var watcher = new FileSystemWatcher(directoryPath))
+            {
+                watcher.NotifyFilter = NotifyFilters.FileName | NotifyFilters.LastWrite | NotifyFilters.DirectoryName;
+                watcher.Filter = "*.*";
+                watcher.Changed += OnChanged;
+                watcher.Created += OnChanged;
+                watcher.Deleted += OnChanged;
+                watcher.EnableRaisingEvents = true;
+                Console.WriteLine("Нажмите 'q' для выхода.");
+                while (Console.Read() != 'q') ;
+            }
+        }
+
+        void OnChanged(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine($"Файл: {e.FullPath} был {e.ChangeType}");
+            Build();
+        }
+    }
+}
